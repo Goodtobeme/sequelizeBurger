c0ab6c9148c83795397dcacec595f536da71bc40
Goodtobeme <eugenebeckwith@ymail.com>
2018-03-17 07:27:26 -0500

    Adding all swquelizeBurger files

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+
+
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define('Burger', {
+    
+    burger_name:{ 
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    devoured: { 
+      allowNull: false,
+      type: DataTypes.BOOLEAN
+  },
+
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+        Burger.hasOne(models.Customer);
+      }
+    }
+  });
+  return Burger;
+};
---
+++ models/customers.js
@@ -0,0 +1,18 @@
+
+
+
+module.exports = function(sequelize, DataTypes) {
+  var customers = sequelize.define('customers', {
+    
+    customer: { 
+      allowNull: false,
+      type: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return customers;
+};
